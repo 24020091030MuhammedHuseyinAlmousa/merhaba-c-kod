37dae56e57ac5965cc6f126afac6107817b5d039
24020091030MuhammedHuseyinAlmousa <huso4university@gmail.com>
2026-02-22 16:21:09 +0300

    Update merhaba.c

--- merhaba.c
+++ merhaba.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main() {
 
-    printf("\n\nMerhaba, iletisim ve sunum teknikleri dersine hosgeldiniz!\n\n");
+    SetConsoleOutputCP(65001); // UTF-8 Türkçe karakter desteği
+
+    printf("\n\nMerhaba, iletişim ve sunum teknikleri dersine hoş geldiniz!\n\n");
     
     return 0;
 }
